f9c82da962e803ab9bcbf63a5373ce20766d07f7
bruz-11 <b.ruz@duocuc.cl>
2026-06-15 19:34:23 -0400

    docs: agregar especificacion Swagger YAML

--- microservicio-login/src/main/java/com/duoc/auth/controller/AuthController.java
+++ microservicio-login/src/main/java/com/duoc/auth/controller/AuthController.java
@@ -1,11 +1,16 @@
 package com.duoc.auth.controller;
 
-import com.duoc.auth.dto.UserDTO;
-import com.duoc.auth.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.duoc.auth.dto.UserDTO;
+import com.duoc.auth.service.AuthService;
 
 @RestController
 @RequestMapping("/api/auth")
